a0caa9db337f3ca064d143ebbb59857d9ed25a2a
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-14 15:00:07 +0700

    create calculate volume and explicit intent

---
+++ app/src/main/java/com/example/androiddasar/IntentApp.kt
@@ -0,0 +1,20 @@
+package com.example.androiddasar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class IntentApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_app)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+
+        btnMoveActivity.setOnClickListener {
+            val moveIntent = Intent(this, MoveActivity::class.java)
+            startActivity(moveIntent)
+        }
+    }
+}
