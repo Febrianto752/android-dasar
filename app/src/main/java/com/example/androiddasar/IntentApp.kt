1264563b1937b00367c8ddffc6f564cd1aacc232
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-15 07:57:25 +0700

    intent move for result

--- app/src/main/java/com/example/androiddasar/IntentApp.kt
+++ app/src/main/java/com/example/androiddasar/IntentApp.kt
@@ -5,8 +5,20 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class IntentApp : AppCompatActivity() {
+    private lateinit var tvResult: TextView
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == MoveForResultActivity.RESULT_CODE && result.data != null) {
+            val selectedValue =
+                result.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
+            tvResult.text = "Hasil : $selectedValue"
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_app)
@@ -58,6 +70,15 @@ class IntentApp : AppCompatActivity() {
             val phoneNumber = "081210841382"
             val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
             startActivity(dialPhoneIntent)
+        }
+
+        // get back pass data intent
+        val btnMoveForResult:Button = findViewById(R.id.btn_move_for_result)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnMoveForResult.setOnClickListener {
+            val moveForResultIntent = Intent(this, MoveForResultActivity::class.java)
+            resultLauncher.launch(moveForResultIntent)
         }
     }
 }
